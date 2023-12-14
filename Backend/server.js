85dbe80ab984c1ff9aca374cffaee7b00e8e749d
elias-sc <152857105+elias-sc@users.noreply.github.com>
2023-12-14 20:54:43 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -107,6 +107,6 @@ app.post('/get-completed-exercises', async (req, res) => {
     });
 });
 
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {console.log("Server started on port 3000")});
+app.listen(port, () => {console.log("Server started on port " + port)});
